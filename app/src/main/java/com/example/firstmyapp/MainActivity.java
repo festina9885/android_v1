aa5bc47574d1d56725083321d4d9ec1405734c77
함동빈 <festina9885@gmail.com>
2021-12-14 17:50:21 +0900

    2일차 수업

--- app/src/main/java/com/example/firstmyapp/MainActivity.java
+++ app/src/main/java/com/example/firstmyapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_layout_challenge_01);
+        setContentView(R.layout.mytube_layout01);
     }
 }
